113c9f4ee26ff8ce8bb45d32ab1d9d71677e4d2e
Senait <mengeshasenait695gmail.com>
2023-01-04 02:06:00 +0900

    feat: add analysis route

---
+++ app/api/journal/[id]/analysis/route.ts
@@ -0,0 +1,32 @@
+import { getUserByClerkId } from '@/services/auth'
+import prisma from '@/services/db'
+import { NextResponse } from 'next/server'
+import { analyzeEntry } from '@/services/ai'
+
+export const PATCH = async (req: Request, { params }) => {
+  const { content } = await req.json()
+  const analysis = await analyzeEntry(content)
+
+  const user = await getUserByClerkId()
+  if (!user) {
+    console.log('user not found')
+    return NextResponse.json({ error: 'user not found'})
+  }
+
+  const updatedAnalysis = await prisma.entryAnalysis.update({
+    where: {
+      entryId: params.id,
+      userId: user.id,
+    },
+    data: {
+      mood: analysis.mood,
+      subject: analysis.subject,
+      summary: analysis.summary,
+      color: analysis.color,
+      emotion: analysis.emotion,
+      sentimentScore: analysis.sentimentScore,
+      emoji: analysis.emoji || '😐',
+    },
+  })
+  return NextResponse.json({ data: updatedAnalysis })
+}
