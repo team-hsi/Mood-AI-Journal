885cd5872eadc6556b3e70344496c34d03c0b240
Senait <mengeshasenait695gmail.com>
2023-01-04 10:20:00 +0900

    feat: add entry routes

---
+++ app/api/journal/route.ts
@@ -0,0 +1,54 @@
+import { getUserByClerkId } from '@/services/auth'
+import prisma from '@/services/db'
+import { revalidatePath } from 'next/cache'
+import { NextResponse } from 'next/server'
+
+export const POST = async () => {
+  const user = await getUserByClerkId()
+  if (!user) {
+    console.log('user not found')
+    return NextResponse.json({ error: 'user not found'})
+  }
+
+  const entry = await prisma.journalEntry.create({
+    data: {
+      userId: user.id,
+      content: 'Write about your day...!',
+      analysis: {
+        create: {
+          userId: user.id,
+          mood: 'not analyzed',
+          subject: 'not analyzed',
+          summary: 'No summary available',
+          color: '#808080',
+          sentimentScore: 0,
+          emoji: '😐',
+        },
+      },
+    },
+    include: { analysis: true },
+  })
+
+  revalidatePath('/journal')
+  return NextResponse.json({ data: entry })
+}
+
+export const GET = async () => {
+  const user = await getUserByClerkId()
+  if (!user) {
+    console.log('user not found')
+    return NextResponse.json({ error: 'user not found'})
+  }
+
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      updatedAt: 'desc',
+    },
+    include: { analysis: true },
+  })
+
+  return NextResponse.json({ data: entries })
+}
