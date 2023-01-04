6a71638246cb2903af9219644ac792b0b49619f8
Senait <mengeshasenait695gmail.com>
2023-01-04 12:18:00 +0900

    feat: add update entry route

---
+++ app/api/journal/[id]/route.ts
@@ -0,0 +1,30 @@
+import { getUserByClerkId } from '@/services/auth'
+import prisma from '@/services/db'
+import { revalidatePath } from 'next/cache'
+import { NextResponse } from 'next/server'
+
+export const PATCH = async (req: Request, { params }) => {
+  const { content } = await req.json()
+
+  const user = await getUserByClerkId()
+  if (!user) {
+    console.log('user not found')
+    return NextResponse.json({ error: 'user not found'})
+  }
+
+  const updatedEntry = await prisma.journalEntry.update({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id: params.id,
+      },
+    },
+    data: {
+      content,
+    },
+    include: { analysis: true },
+  })
+
+  revalidatePath(`/journal/${params.id}`)
+  return NextResponse.json({ data: updatedEntry })
+}
