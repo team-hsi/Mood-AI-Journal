6665daf22f1332f5770b24f80cb790abba8b03ab
Hunde-D <hunde.ddh@gmail.com>
2023-01-06 10:16:24 +0300

    feat(api): add api helper functions

---
+++ services/api.ts
@@ -0,0 +1,59 @@
+const createURL = (path: string) => {
+  return window.location.origin + path
+}
+
+export const createNewEntry = async () => {
+  try {
+    const res = await fetch(
+      new Request(createURL('/api/journal'), {
+        method: 'POST',
+      }),
+    )
+
+    if (res.ok) {
+      const data = await res.json()
+      return data.data
+    }
+  } catch (error) {
+    console.log('error:', error)
+    return null
+  }
+}
+
+export const updateEntry = async (id: string, content: string) => {
+  const res = await fetch(
+    new Request(createURL(`/api/journal/${id}`), {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ content }),
+    }),
+  )
+
+  if (res.ok) {
+    const data = await res.json()
+    return data.data
+  }
+}
+export const updateAnalysis = async (id: string, content: string) => {
+  try {
+    const res = await fetch(
+      new Request(createURL(`/api/journal/${id}/analysis`), {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ content }),
+      }),
+    )
+
+    if (res.ok) {
+      const data = await res.json()
+      return data.data
+    }
+  } catch (error) {
+    console.log('error:', error)
+    return null
+  }
+}
