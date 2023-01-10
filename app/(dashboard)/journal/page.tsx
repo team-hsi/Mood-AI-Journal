8c451227544b75a5a17ac7461a220939d38a5514
Imanmoh99 <imanahmedmoh2001@ethioware.org>
2023-01-10 06:19:27 +0300

    feat: Journal part of dashboard

---
+++ app/(dashboard)/journal/page.tsx
@@ -0,0 +1,22 @@
+import EntryCard from '@/components/entry-card'
+import NewEntry from '@/components/new-entry'
+import { getEntries } from '@/utils/action'
+import { ScrollArea } from '@/components/ui/scroll-area'
+
+const Page = async () => {
+  const entries = await getEntries()
+
+  return (
+    <div className="flex h-full w-full flex-col gap-5">
+      <h2 className="text-3xl">Journals</h2>
+      <NewEntry />
+      <ScrollArea className="h-[200px] w-full flex-auto">
+        <div className="grid gap-5 pb-3 md:flex md:flex-wrap">
+          <EntryCard entries={entries} />
+        </div>
+      </ScrollArea>
+    </div>
+  )
+}
+
+export default Page
