ae6ea7d849ccff6927903d33dd79b6073d506f6b
Senait Mengesha <mengeshasenait695@gmail.com>
2023-01-19 12:34:56 +0300

    refactor: buil erorrs

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,6 +1,10 @@
 'use client'
 
-import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable'
 import { Button } from '@/components/ui/button'
 import MobileAnalysisView from '@/components/mobile-analysis-view'
 import { AnalysisPanel } from '@/components/analysis-panel'
@@ -8,22 +12,17 @@ import { ContentEditor } from '@/components/content-editor'
 import { analysisData } from '@/services/types'
 import { useEditor } from '@/hooks/use-editor'
 import { hexToRGB } from '@/services/color'
-import { Analysis } from '@/services/types'
 
-interface EditorProps {
-  entry: {
-    id: string
-    content: string
-    analysis: Analysis
-  }
-}
-
-export default function Editor({ entry }: EditorProps) {
-  const { content, setContent, analysis, analysisLoading, handleNewAnalysis } = useEditor(entry)
+export default function Editor({ entry }) {
+  const { content, setContent, analysis, analysisLoading, handleNewAnalysis } =
+    useEditor(entry)
 
   return (
     <ResizablePanelGroup direction="horizontal" className="h-full w-full">
-      <ResizablePanel defaultSize={75} className="px-3 py-1 max-sm:flex max-sm:flex-col max-sm:gap-4 max-sm:pl-2 max-sm:pr-8">
+      <ResizablePanel
+        defaultSize={75}
+        className="px-3 py-1 max-sm:flex max-sm:flex-col max-sm:gap-4 max-sm:pl-2 max-sm:pr-8"
+      >
         <ContentEditor
           content={content}
           onChange={setContent}
@@ -58,4 +57,3 @@ export default function Editor({ entry }: EditorProps) {
     </ResizablePanelGroup>
   )
 }
-
