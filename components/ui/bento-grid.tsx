8e98495b086dd2093b1c66b7381f945626dadde8
Hunde-D <hunde.ddh@gmail.com>
2024-12-23 00:58:51 +0300

    refactor: disable some eslint

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -1,36 +1,37 @@
-import { ReactNode } from "react";
-import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { ReactNode } from 'react'
+import { ArrowRightIcon } from '@radix-ui/react-icons'
 
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
 
 type BentoGridProps = {
-  children: ReactNode;
-  className?: string;
-};
+  children: ReactNode
+  className?: string
+}
 
 type BentoCardProps = {
-  name: string;
-  className: string;
-  background: ReactNode;
-  Icon: any;
-  description: string;
-  href: string;
-  cta: string;
-};
+  name: string
+  className: string
+  background: ReactNode
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Icon: any
+  description: string
+  href: string
+  cta: string
+}
 
 const BentoGrid = ({ children, className }: BentoGridProps) => {
   return (
     <div
       className={cn(
-        "grid w-full auto-rows-[22rem] grid-cols-3 gap-4",
+        'grid w-full auto-rows-[22rem] grid-cols-3 gap-4',
         className,
       )}
     >
       {children}
     </div>
-  );
-};
+  )
+}
 
 const BentoCard = ({
   name,
@@ -44,11 +45,11 @@ const BentoCard = ({
   <div
     key={name}
     className={cn(
-      "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
+      'group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl',
       // light styles
-      "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+      'bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]',
       // dark styles
-      "transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+      'transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]',
       className,
     )}
   >
@@ -63,7 +64,7 @@ const BentoCard = ({
 
     <div
       className={cn(
-        "pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100",
+        'pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100',
       )}
     >
       <Button variant="ghost" asChild size="sm" className="pointer-events-auto">
@@ -75,6 +76,6 @@ const BentoCard = ({
     </div>
     <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10" />
   </div>
-);
+)
 
-export { BentoCard, BentoGrid };
+export { BentoCard, BentoGrid }
